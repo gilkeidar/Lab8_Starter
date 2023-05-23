fa5906ecc052cb5c0daa4ac513158ef2b9c0af6b
Gil Keidar <keidar.gil@gmail.com>
2023-05-22 21:35:08 -0700

    Completed expose and explore

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # Lab 8 - Starter
+
+1.  Where would you fit your automated tests in your Recipe project development pipeline? Select one of the following and explain why.
+
+    1. Within a GitHub action that runs whenever code is pushed.
+
+    By doing this, I can write the test suite once (and modify it when needed),
+    and know that whenever I push to the repository, the test suite will be run.
+    This makes it less tedious as I don't have to manually run the tests for every change.
+
+    Waiting to run the test suites after all development is completed could be disastrous as bugs could fester and at the end, I could find a really hard to fix bug as it's at the foundation of a lot of other code.
+
+2. Would you use an end to end test to check if a function is returning the correct output? (yes/no)
+
+    No, it would be more appropriate to use unit testing for this case.
+
+3. Would you use a unit test to test the “message” feature of a messaging application? Why or why not? For this question, assume the “message” feature allows a user to write and send a message to another user.
+
+    Since this feature tests the interaction between individual components (i.e., want to check that the text the user wrote reached the other user) instead of testing an individual part by itself, I would use E2E testing instead of unit testing for this case.
+
+4. Would you use a unit test to test the “max message length” feature of a messaging application? Why or why not? For this question, assume the “max message length” feature prevents the user from typing more than 80 characters.
+
+    No, I would use E2E for this as well (I think this could be done by filling the text field with 80 characters, checking that the text filled so far matches what was sent, typing just one more character, and checking that the text filled so far matches the original text).
+
+
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -3,3 +3,88 @@
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
 // TODO - Part 2
+
+//  isPhoneNumber
+test("123-456-7890 is a phone number", () => {
+    expect(functions.isPhoneNumber("123-456-7890")).toBe(true);
+})
+
+test("000-000-0000 is a phone number", () => {
+    expect(functions.isPhoneNumber("000-000-0000")).toBe(true);
+})
+
+test("1 is not a phone number", () => {
+    expect(functions.isPhoneNumber("1")).toBe(false);
+})
+
+test("abcd is not a phone number", () => {
+    expect(functions.isPhoneNumber("abcd")).toBe(false);
+})
+
+//  isEmail
+test("a@b.com is a valid email", () => {
+    expect(functions.isEmail("a@b.com")).toBe(true);
+})
+
+test("cloudstrife@finalfantasy.com is a valid email", () => {
+    expect(functions.isEmail("cloudstrife@finalfantasy.com")).toBe(true);
+})
+
+test("toboldlygo is not a valid email", () => {
+    expect(functions.isEmail("toboldlygo")).toBe(false);
+})
+
+test("123-456-7890 is not a valid email", () => {
+    expect(functions.isEmail("123-456-7890")).toBe(false);
+})
+
+//  isStrongPassword
+test("Mellon is a strong password", () => {
+    expect(functions.isStrongPassword("Mellon")).toBe(true);
+})
+
+test("password is a strong password", () => {
+    expect(functions.isStrongPassword("abcde")).toBe(true);
+})
+
+test("SDfjsdf35r!3fksghldaet4 is not a strong password", () => {
+    expect(functions.isStrongPassword("SDfjsdf35r!3fksghldaet4")).toBe(false);
+})
+
+test("1234 is not a strong password", () => {
+    expect(functions.isStrongPassword("1234")).toBe(false);
+})
+
+//`isDate
+test("05/22/2023 is a valid date", () => {
+    expect(functions.isDate("05/22/2023")).toBe(true);
+})
+
+test("1/1/1970 is a valid date", () => {
+    expect(functions.isDate("1/1/1970")).toBe(true);
+})
+
+test("1 / 1 / 1970 is not a valid date", () => {
+    expect(functions.isDate("1 / 1 / 1970")).toBe(false);
+})
+
+test("A long time ago in a galaxy far, far away... is not a valid date", () => {
+    expect(functions.isDate("A long time ago in a galaxy far, far away...")).toBe(false);
+})
+
+//  isHexColor
+test("#000000 is a valid hex color", () => {
+    expect(functions.isHexColor("#000000")).toBe(true);
+})
+
+test("#FFFFFF is a valid hex color", () => {
+    expect(functions.isHexColor("#FFFFFF")).toBe(true);
+})
+
+test("red is not a valid hex color", () => {
+    expect(functions.isHexColor("red")).toBe(false);
+})
+
+test("The pale blue of Luke Skywalker's lightsaber, before it was digitally remastered is not a valid hex color", () => {
+    expect(functions.isHexColor("The pale blue of Luke Skywalker's lightsaber, before it was digitally remastered")).toBe(false);
+})
